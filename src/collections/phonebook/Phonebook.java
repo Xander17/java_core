e94f369d08a5f62ebb3792b504344aacbcf1349e
Cromag <alseko_2001@mail.ru>
2019-09-24 07:17:25 +0300

    phonenumber -> hashlist

--- src/collections/phonebook/Phonebook.java
+++ src/collections/phonebook/Phonebook.java
@@ -1,31 +1,33 @@
 package collections.phonebook;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 
 public class Phonebook {
-    private Map<String, String> phonebook;
-    private final String separator = "_";
+    private Map<String, HashSet<String>> phonebook;
 
     public Phonebook() {
         phonebook = new HashMap<>();
     }
 
     public void add(String lastName, String phoneNumber) {
-        int count = 0;
-        while (phonebook.containsKey(lastNameEntryFormat(lastName, count))) count++;
-        phonebook.put(lastNameEntryFormat(lastName, count), phoneNumber);
-    }
-
-    public void get(String lastName) {
-        for (Map.Entry<String, String> entry : phonebook.entrySet()) {
-            String key = entry.getKey();
-            if (key.substring(0, key.indexOf(separator)).equalsIgnoreCase(lastName))
-                System.out.println(entry.getValue());
+        lastName = lastName.toLowerCase();
+        if (phonebook.containsKey(lastName))
+            phonebook.get(lastName).add(phoneNumber);
+        else {
+            HashSet<String> set = new HashSet<>();
+            set.add(phoneNumber);
+            phonebook.put(lastName.toLowerCase(), set);
         }
     }
 
-    private String lastNameEntryFormat(String lastName, int count) {
-        return lastName + separator + count;
+    public void get(String lastName) {
+        lastName = lastName.toLowerCase();
+        if (phonebook.containsKey(lastName)) {
+            for (String number : phonebook.get(lastName)) {
+                System.out.println(number);
+            }
+        } else System.out.println("В телефонной книге нет записей с такой фамилией");
     }
 }
