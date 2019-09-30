472ec3a237a79068899e543047703fb67410de2c
Cromag <alseko_2001@mail.ru>
2019-09-30 22:11:25 +0300

    add threads lesson

---
+++ src/threads/Main.java
@@ -0,0 +1,19 @@
+package threads;
+
+import java.util.Arrays;
+
+public class Main {
+    static final int ARRAY_SIZE = 10000000;
+    static final int SUB_ARRAY_NUM = 2;
+    static float[] array = new float[ARRAY_SIZE];
+
+    public static void main(String[] args) {
+
+
+    }
+
+    private void fillArray(float[] arr) {
+        Arrays.fill(arr, 1);
+    }
+
+}
